23713ce4fa2b9b371e4967e676d75a9fafd31401
Sergij Rylskyj <sergij.r@scopicsoftware.com>
2024-05-02 20:28:51 +0300

    added directory listing

---
+++ os_utils.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+
+def handle_directory(source_path, destination_path):
+    if not os.path.exists(source_path):
+        return print(f'Such directory {source_path} doesn\'t exist')
+
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path, exist_ok=True)
+    
+    for entry in os.listdir(source_path):
+        if entry.startswith('.') or entry.startswith('__'):
+            continue
+
+        process_entry(entry, source_path, destination_path)
+
+def process_entry(entry, source_path, destination_path):
+    entry_src_path = os.path.join(source_path, entry)
+    print('Processing:', entry_src_path)
+        
+    if os.path.isdir(entry_src_path):
+        new_destination_path = os.path.join(destination_path, entry)
+        os.makedirs(new_destination_path, exist_ok=True)
+        handle_directory(entry_src_path, new_destination_path)
+    else:
+        copy_file_to_extension_folder(entry_src_path, destination_path)
+
+def copy_file_to_extension_folder(file_path, destination_path):
+    filename = os.path.basename(file_path)
+    extension = os.path.splitext(filename)[1].strip('.')
+    directory_path = os.path.join(destination_path, extension) if extension else destination_path
+    
+    os.makedirs(directory_path, exist_ok=True)  # Ensure directory exists
+
+    dst_file_path = os.path.join(directory_path, filename)
+    try:
+        shutil.copy(file_path, dst_file_path)
+        print(f'Copied {file_path} to {dst_file_path}')
+    except IOError as e:
+        print(f"Error copying {file_path} to {dst_file_path}: {e}")
+        
